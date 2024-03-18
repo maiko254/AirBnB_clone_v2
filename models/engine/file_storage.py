1eaff9a420d04553bc57d538eb03b6a3451b6d67
maiko254 <bonyomichael406.com>
2024-03-18 20:01:18 +0300

    added delete method to delete objects in file storage and modified all method to return dictionary of filtered objects according to class provided

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            cls_dict = {}
+            for k, v in FileStorage.__objects.items():
+                cls_name = k.split('.')[0]
+                if cls_name == cls.__name__:
+                    cls_dict.update({k: v})
+            return cls_dict
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +53,12 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Deletes object from FileStorage.__objects dictionary """
+        if obj:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            FileStorage.__objects.pop(key)
