7a98d49db50b97d60720bf498380d43cb8889e54
maiko254 <bonyomichael406.com>
2024-03-20 13:05:39 +0300

    mapped class Place to table places and added relationship linking it to tables users and cities

--- models/city.py
+++ models/city.py
@@ -3,11 +3,17 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import models
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id', ondelete='CASCADE'),
-                      nullable=False)
+    if models.storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id',
+                          ondelete='CASCADE'), nullable=False)
+        places = relationship("Place", cascade="all, delete", backref="cities")
+    else:
+        state_id = ""
+        name = ""
