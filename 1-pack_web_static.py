ad6e12b01b1847410fba664fca253f75d1ee62fd
maiko254 <bonyomichael406.com>
2024-04-04 13:55:42 +0300

    fabric script to create a .tgz archive containing all web_static files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+   A fabric script that compresses the contents of web_static into a .tgz
+   archive to be deployed to a web server
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    try:
+        local('mkdir versions')
+        now = datetime.now()
+
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static" + timestamp + ".tgz"
+
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        return("versions/{}".format(archive_name))
+    except Exception:
+        return(None)
