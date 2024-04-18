14d3c651c4efb5e737284d20b6352e3afb0667a1
maiko254 <bonyomichael406.com>
2024-04-18 15:30:07 +0300

    Starts a Flask web application listening on 0.0.0.0 and port 5000

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Starts a Flask web application listening on 0.0.0.0 and port 5000 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Defines a route that prints a message when / is requested """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    """ Main function """
+    app.run(host='0.0.0.0', port=5000)
