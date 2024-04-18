e6d479d312a43ac14928ee8c5fb651311659d396
maiko254 <bonyomichael406.com>
2024-04-18 15:39:02 +0300

    Starts a Flask web application with routes / and /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Starts a Flask web application listening on 0.0.0.0 and port 5000 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Defines a route that prints a message when / is requested """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Prints HBNB when /hbnb route is requested """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """ Main function """
+    app.run(host='0.0.0.0', port=5000)
